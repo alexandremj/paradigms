c26b865300f942f98a898666e2b846914f3727cd
Alexandre Muller Junior <alexandremj@pm.me>
2020-03-12 14:19:41 -0300

    [WIP] always returns False

---
+++ haskell/aula1/problema17.hs
@@ -0,0 +1,20 @@
+-- wrong answer
+-- returns False everytime
+
+isPrime :: Int -> Int -> Bool
+
+isPrime x 0 = True
+isPrime x y =
+    if (x `mod` y == 0) then
+        False
+    else
+        isPrime x (y-1)
+
+
+main = do
+    xStr <- getLine
+
+    let x = (read xStr :: Int)
+
+    let result = isPrime x (x-1)
+    print result
